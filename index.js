90ae60706385356c9b788649deed4f319be4b4a9
UmidYul <yumid257@gmail.com>
2024-08-08 14:46:22 +0500

    server is created v1.1

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express()
 const PORT = 3000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
