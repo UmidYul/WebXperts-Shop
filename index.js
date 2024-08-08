c835a33c8b8b7275f80a77cefd6bbccd6e326e0c
UmidYul <yumid257@gmail.com>
2024-08-08 14:41:54 +0500

    server created

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hi");
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express()
+const PORT = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + "/public/"))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/main.html")
+})
+
+app.listen(PORT, () => console.log("http://localhost:" + PORT))
